4f9e348b690422e5cd4f8125ed00d7f9c39de7de
lschlestein <lschlestein@gmail.com>
2023-06-21 21:35:19 -0300

    Implementados testes, e também uma classe Service

--- src/test/java/com/batch/springbatch/SpringBatchExampleApplicationTests.java
+++ src/test/java/com/batch/springbatch/SpringBatchExampleApplicationTests.java
@@ -3,11 +3,28 @@ package com.batch.springbatch;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 @SpringBootTest
 class SpringBatchExampleApplicationTests {
-
+Calculator calculator = new Calculator();
     @Test
-    void contextLoads() {
-    }
+void itShouldAddNumbers(){
+        // given
+        int numberOne = 15;
+        int numberTwo = 10;
 
+        // when
+        int result = calculator.add(numberOne,numberTwo);
+
+        // then
+
+        assertThat(result).isEqualTo(25);
+}
+
+class Calculator{
+    int add(int a, int b){
+        return a+b;
+    }
+}
 }
